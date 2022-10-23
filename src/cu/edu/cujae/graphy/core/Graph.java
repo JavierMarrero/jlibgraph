d268fec0d22f1ed4d909e234cc35181dcc2db84c
Javier Marrero <jv.freelancerdevteam.qba@gmail.com>
2022-10-23 00:19:05 -0400

    Added an abstract graph interface to test the production process.

---
+++ src/cu/edu/cujae/graphy/core/Graph.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2022 CUJAE.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package cu.edu.cujae.graphy.core;
+
+/**
+ * The <code>Graph</code> interface represents a graph as an abstract
+ * data structure. The graph does not make a distinction with the data
+ * type it holds, it just presents an abstract model of a graph.
+ *
+ * @author Javier Marrero
+ */
+public interface Graph<T>
+{
+    
+}
