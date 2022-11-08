0ff0795f2c6c7f539f03deb8b6ecc816ddb7cb97
Jose <Jose@DESKTOP-HKDHM8O>
2022-11-08 11:35:05 +0100

    Added a new tree detection algorithm.

---
+++ src/cu/edu/cujae/graphy/algorithms/TreeDetection.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2022 Jose.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package cu.edu.cujae.graphy.algorithms;
+
+import cu.edu.cujae.graphy.core.Graph;
+
+/**
+ *
+ * @author Jose
+ * @param <V>
+ */
+public class TreeDetection<V> extends AbstractAlgorithm<Boolean>
+{
+    
+    public TreeDetection(Graph<V> graph)
+    {
+        
+    }
+
+    @Override
+    public Algorithm<Boolean> apply() {
+        return this;
+    }
+    
+}
