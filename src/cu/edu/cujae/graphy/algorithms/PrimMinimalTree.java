7f7064ab944b73404f08f06a3eee87a5a3d89c83
Carlos Robaina <cdrobayna01@gmail.com>
2022-11-10 23:42:36 -0500

    Working on Prim

--- src/cu/edu/cujae/graphy/algorithms/PrimMinimalTree.java
+++ src/cu/edu/cujae/graphy/algorithms/PrimMinimalTree.java
@@ -1,20 +1,34 @@
 package cu.edu.cujae.graphy.algorithms;
 
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.function.Consumer;
+
+import cu.edu.cujae.graphy.core.Edge;
 import cu.edu.cujae.graphy.core.Graph;
 import cu.edu.cujae.graphy.core.Tree;
 import cu.edu.cujae.graphy.core.iterators.GraphIterator;
 import cu.edu.cujae.graphy.core.trees.DefaultGeneralTree;
+import cu.edu.cujae.graphy.core.utility.GraphBuilders;
+import cu.edu.cujae.graphy.core.utility.Weights;
 
 public class PrimMinimalTree<T> extends AbstractAlgorithm<Tree<T>>{
 
-    GraphIterator<T> iterator;
+    private Graph<T> graph;
+    private int start;
 
     public PrimMinimalTree(Graph<T> graph) {
         this(graph, 0);
     }
 
+    @SuppressWarnings("unchecked")
     public PrimMinimalTree(Graph<T> graph, int rootLabel) {
-        super(new DefaultGeneralTree<T>());
+        super((Tree<T>)GraphBuilders.makeSimpleWeightedGraph(true));
 
         if(!graph.isWeighted()){
             throw new IllegalArgumentException("The graph is not weighted.");
@@ -23,17 +37,32 @@ public class PrimMinimalTree<T> extends AbstractAlgorithm<Tree<T>>{
             throw new IllegalArgumentException("The graph is directed.");
         }
 
-        iterator.getAllAdjacentEdges();
+        // TODO Check Connected Graph
 
-        iterator = graph.iterator(rootLabel);
-        getResult().add(iterator.getLabel(), iterator.next());
+        this.graph = graph;
+        this.start = rootLabel;
     }
 
     @Override
     public Algorithm<Tree<T>> apply() {
+        GraphIterator<T> randomIter = graph.iterator(start);
+        GraphIterator<T> iterator = (GraphIterator<T>)graph.depthFirstSearchIterator(false);
+        PriorityQueue<Edge> queue =  new PriorityQueue<>(new Comparator<Edge>() {
+            @Override
+            public int compare(Edge a, Edge b) {
+                return 0;
+            }
+            
+        });
+        
+        while(iterator.hasNext()){
+            get().add(iterator.next());
+        }
 
+        for(int i = 0; i < graph.size(); i++){
 
-        // TODO Auto-generated method stub
-        return null;
+        }
+
+        return this;
     }    
 }
