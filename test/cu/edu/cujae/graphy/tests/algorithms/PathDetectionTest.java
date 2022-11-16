87a012552c32ffe5ad7ccb8433f567981146a124
Carlos Robaina <cdrobayna01@gmail.com>
2022-11-15 23:25:53 -0500

    Removed JUnit Test Cases

--- test/cu/edu/cujae/graphy/tests/algorithms/PathDetectionTest.java
+++ test/cu/edu/cujae/graphy/tests/algorithms/PathDetectionTest.java
@@ -1,20 +1,13 @@
 package cu.edu.cujae.graphy.tests.algorithms;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
 import cu.edu.cujae.graphy.algorithms.PathDetection;
 import cu.edu.cujae.graphy.core.Graph;
 import cu.edu.cujae.graphy.core.utility.GraphBuilders;
 
 public class PathDetectionTest {
 
-    private Graph<Integer> graph;
-
-    private void initialize(){
-        graph = GraphBuilders.makeSimpleGraph(true);
+    public static void main(String[] args) {
+        Graph<Integer> graph = GraphBuilders.makeSimpleGraph(true);
 
         graph.add(0);
         graph.add(1);
@@ -29,23 +22,7 @@ public class PathDetectionTest {
         graph.connect(1, 2);
         graph.connect(1, 4);
         graph.connect(4, 5);
-    }
-
-    @Test
-    public void testTruePath(){
-        initialize();
-        assertTrue(new PathDetection<>(graph, 0, 4).apply().get());
-    }
-
-    @Test
-    public void testDeadNode(){
-        initialize();
-        assertFalse(new PathDetection<>(graph, 3, 4).apply().get());
-    }
-
-    @Test 
-    public void testBackTracking(){
-        initialize();
-        assertTrue(new PathDetection<>(graph, 0, 5).apply().get());
+    
+        System.out.println(new PathDetection<>(graph, 1, 3).apply().get());
     }
 }
--- test/cu/edu/cujae/graphy/tests/algorithms/PrimTest.java
+++ test/cu/edu/cujae/graphy/tests/algorithms/PrimTest.java
@@ -1,80 +1,11 @@
 package cu.edu.cujae.graphy.tests.algorithms;
 
-import static org.junit.Assert.assertThrows;
-
-import org.junit.Test;
-
 import cu.edu.cujae.graphy.algorithms.PrimMinimalTree;
-import cu.edu.cujae.graphy.core.Graph;
 import cu.edu.cujae.graphy.core.WeightedGraph;
 import cu.edu.cujae.graphy.core.utility.GraphBuilders;
 import cu.edu.cujae.graphy.core.utility.Weights;
 
 public class PrimTest {
-
-    private WeightedGraph<Character> graph;
-
-    @Test
-    public void directedGraphTest() {
-        initialize(true, true);
-        assertThrows(IllegalArgumentException.class, () -> {
-            System.out.println(new PrimMinimalTree<>(graph).apply().get().toString());
-        });
-    }
-
-    @Test
-    public void weightedGraphTest() {
-        Graph<Character> graph = GraphBuilders.makeSimpleGraph(false);
-        assertThrows(IllegalArgumentException.class, () -> {
-            System.out.println(new PrimMinimalTree<>(graph).apply().get().toString());
-        });
-    }
-
-    private void initialize(boolean directed, boolean weighted) {
-        graph = GraphBuilders.makeSimpleWeightedGraph(directed);
-
-            graph.add(0, 'a');
-            graph.add(1, 'b');
-            graph.add(2, 'c');
-            graph.add(3, 'd');
-            graph.add(4, 'e');
-            graph.add(5, 'f');
-            graph.add(6, 'g');
-            graph.add(7, 'h');
-            graph.add(8, 'i');
-
-            // Outside
-            graph.connect(0, 1, Weights.makeWeight(2));
-            graph.connect(1, 2, Weights.makeWeight(3));
-            graph.connect(2, 5, Weights.makeWeight(4));
-            graph.connect(5, 8, Weights.makeWeight(5));
-            graph.connect(8, 7, Weights.makeWeight(3));
-            graph.connect(7, 6, Weights.makeWeight(4));
-            graph.connect(6, 3, Weights.makeWeight(6));
-            graph.connect(3, 0, Weights.makeWeight(1));
-
-            // Crux
-            graph.connect(3, 4, Weights.makeWeight(4));
-            graph.connect(1, 4, Weights.makeWeight(2));
-            graph.connect(5, 4, Weights.makeWeight(1));
-            graph.connect(7, 4, Weights.makeWeight(2));
-
-            // FirstInside
-            graph.connect(0, 4, Weights.makeWeight(2));
-            graph.connect(3, 1, Weights.makeWeight(3));
-
-            // SecondInside
-            graph.connect(1, 5, Weights.makeWeight(3));
-            graph.connect(2, 4, Weights.makeWeight(4));
-
-            // ThirdInside
-            graph.connect(3, 7, Weights.makeWeight(5));
-            graph.connect(4, 6, Weights.makeWeight(6));
-
-            // ForthInside
-            graph.connect(4, 8, Weights.makeWeight(3));
-            graph.connect(5, 7, Weights.makeWeight(3));
-    }
 
     // Main Test
     public static void main(String[] args) {
