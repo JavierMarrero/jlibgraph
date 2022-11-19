81c8c7e24be33835f0004875701003e0f04d0624
Cesar <98931642+Cesar0112@users.noreply.github.com>
2022-11-19 17:03:32 -0500

    Agregué la clase algoritmo EulerianCycleDetection
    Para detectar si un grafo contiene al menos un ciclo euleriano.

---
+++ src/cu/edu/cujae/graphy/algorithms/EulerianCycleDetection.java
@@ -0,0 +1,49 @@
+package cu.edu.cujae.graphy.algorithms;
+/** 
+ * @author César Férnandez García
+ * Esta clase permite ver si existe al menos un Ciclo Euleriano
+ * */
+import cu.edu.cujae.graphy.core.Graph;
+
+import cu.edu.cujae.graphy.core.iterators.GraphIterator;
+
+public class EulerianCycleDetection<V> extends AbstractAlgorithm<Boolean> {
+
+	private final Graph<V> graph;
+		
+	public EulerianCycleDetection(Graph<V> graph) {
+		super(true);
+		this.graph = graph;
+	}
+	@Override
+	public Algorithm<Boolean> apply() {
+		//esta funcion lo que hace es ver si deja de cumplirse la paridad de los grados de los nodos
+		boolean isOdd = false;
+		GraphIterator<V> iterator = (GraphIterator<V>) graph.depthFirstSearchIterator(true);
+		while(!isOdd && iterator.hasNext()) {
+			iterator.next();
+			int numberOfEdges = iterator.getAllAdjacentEdges().size();
+			if( (numberOfEdges == 0) || ( (numberOfEdges % 2) == 1) ) {
+				isOdd=true;
+			}	
+			
+							
+		}
+		if(isOdd) {
+			setResult(!isOdd);
+		}
+		
+		return this;
+	}
+	public Graph<V> getGraph() {
+		return graph;
+	}
+	
+	
+}
+
+
+
+
+
+
